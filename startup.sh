43c1cd9415162f3bbc7473483790b3d116d05756
ivanG9 <ivan21valla@gmail.com>
2025-05-19 19:21:25 +0200

    Fix: usar tailwind install correctamente

--- startup.sh
+++ startup.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 set -e
 
-cd theme
-npm install
-cd ..
+# Instala dependencias Tailwind
+python manage.py tailwind install
 
+# Compila Tailwind
 python manage.py tailwind build
+
+# Archivos estáticos y migraciones
 python manage.py collectstatic --noinput
 python manage.py migrate
