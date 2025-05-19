fafc283d0bb00dfb090e92e5fcb9ce9d430e6981
ivanG9 <ivan21valla@gmail.com>
2025-05-19 19:39:02 +0200

    Fix temporal: ignorar tailwind para probar deploy

--- startup.sh
+++ startup.sh
@@ -3,7 +3,7 @@ set -e
 
 cd gastrobot
 
-python manage.py tailwind install
-python manage.py tailwind build
+#python manage.py tailwind install
+#python manage.py tailwind build
 python manage.py collectstatic --noinput
 python manage.py migrate
