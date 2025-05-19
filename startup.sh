035f1c6f500b48d9abc34a567a3cb3e48ffd58ea
ivanG9 <ivan21valla@gmail.com>
2025-05-19 19:27:14 +0200

    Fix: ajustar ruta a manage.py

--- startup.sh
+++ startup.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 set -e
 
-# Instala dependencias Tailwind
+cd gastrobot
+
 python manage.py tailwind install
-
-# Compila Tailwind
 python manage.py tailwind build
-
-# Archivos estáticos y migraciones
 python manage.py collectstatic --noinput
 python manage.py migrate
