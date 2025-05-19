728d8a0ca37b2e90856dd49d27225036faba8456
ivanG9 <ivan21valla@gmail.com>
2025-05-19 19:09:23 +0200

    Railwall V1.2

--- startup.sh
+++ startup.sh
@@ -1,21 +1,10 @@
 #!/bin/bash
-
-# Salir si ocurre algún error
 set -e
 
-# Instalar dependencias de Tailwind
 cd theme
 npm install
 cd ..
 
-# Compilar Tailwind en modo producción
 python manage.py tailwind build
-
-# Recolectar archivos estáticos
 python manage.py collectstatic --noinput
-
-# Aplicar migraciones
 python manage.py migrate
-
-# Ejecutar servidor Gunicorn
-gunicorn gastrobot.wsgi:application --bind 0.0.0.0:8000
